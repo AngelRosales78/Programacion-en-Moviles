d3c901c26acde173f7faf803023dab59f75dd6a3
AngelRosales78 <rosalesmonteroangel@gmail.com>
2026-09-16 16:35:41 -0500

    Se agrego un formulario que agrega productos a una lista observable

--- Semana04/Lab04/app/src/main/java/com/rosalesm/lab04carritotecsup/MainActivity.kt
+++ Semana04/Lab04/app/src/main/java/com/rosalesm/lab04carritotecsup/MainActivity.kt
@@ -3,45 +3,16 @@ package com.rosalesm.lab04carritotecsup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.rosalesm.lab04carritotecsup.ui.theme.Lab04CarritoTecsupTheme
+import androidx.compose.material3.MaterialTheme
+import com.rosalesm.lab04carritotecsup.PantallaCarrito
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Lab04CarritoTecsupTheme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                PantallaCarrito()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab04CarritoTecsupTheme {
-        Greeting("Android")
     }
 }
